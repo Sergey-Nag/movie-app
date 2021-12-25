49e487537cc9ad45439374588b68a764e9e91fc6
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-12-26 00:35:57 +0200

    script to add env variables

---
+++ setVariables.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+if (process.argv.length < 3) throw Error('Variables are required!\nExample: node setVariables var1=val1 var2=val2')
+
+const variables = process.argv
+  .slice(2)
+  .map(arg => arg.split('='))
+  .reduce((acc, [variable, value]) => {
+    acc[variable] = value;
+    return acc;
+  }, {});
+
+variables['production'] = true;
+
+const fileText = `export const environment = ${JSON.stringify(variables)};`
+
+fs.writeFileSync(__dirname + '/src/environments/environments.prod2.ts', fileText, 'utf8');
